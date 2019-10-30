e10e85924764f3478dafeb7189e392dbe9da7984
Mohammad Kiani <mohammadkiani@outlook.com>
2019-10-30 13:32:42 -0400

    add numberofrows and cellforindexpath functions add label and image view and detailed label for the cell

--- CustomFoodTable/ViewController.swift
+++ CustomFoodTable/ViewController.swift
@@ -10,17 +10,27 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var foods: [String]?
+    var calories: [Int]?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        foods = ["Apple", "Banana", "Burger", "Fries", "Orange", "Pizza"]
+        calories = [50, 60, 900, 600, 30, 700]
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return foods?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        guard foods != nil else {return UITableViewCell()}
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "")
+        let foodName = foods![indexPath.row]
+        cell.textLabel?.text = foodName
+        cell.imageView?.image = UIImage(named: foodName)
+        cell.detailTextLabel?.text = "\(calories![indexPath.row])"
+        return cell
     }
 
 
