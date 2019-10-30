55feca58308a7f30a8e5e97d8e4f2b3dbcd49e3c
Mohammad Kiani <mohammadkiani@outlook.com>
2019-10-30 13:24:15 -0400

    add images  as data source

--- CustomFoodTable/ViewController.swift
+++ CustomFoodTable/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 
 
