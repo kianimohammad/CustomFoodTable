7e5e43d72da40f956a84a16f4a232f1c0b8f85b6
Mohammad Kiani <mohammadkiani@outlook.com>
2019-10-30 14:00:15 -0400

    add a subclass of UITableViewCell

---
+++ CustomFoodTable/FoodTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FoodTableViewCell.swift
+//  CustomFoodTable
+//
+//  Created by Mohammad Kiani on 2019-10-30.
+//  Copyright © 2019 mohammadkiani. All rights reserved.
+//
+
+import UIKit
+
+class FoodTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
