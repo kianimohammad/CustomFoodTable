04f6d4f23c2d9c42833f0a3edb5edd2365b37849
Mohammad Kiani <mohammadkiani@outlook.com>
2019-11-01 13:19:41 -0400

    add foddNibCell swift file and xib file

---
+++ CustomFoodTable/FoodNibCell.swift
@@ -0,0 +1,24 @@
+//
+//  FoodNibCell.swift
+//  CustomFoodTable
+//
+//  Created by Mohammad Kiani on 2019-11-01.
+//  Copyright © 2019 mohammadkiani. All rights reserved.
+//
+
+import UIKit
+
+class FoodNibCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
